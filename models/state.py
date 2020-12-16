0c621c87da85279324096f36f77c41adb2988128
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-12-16 14:50:17 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -7,7 +7,7 @@ class State(BaseModel):
     """ State class """
     name = ""
 
-    def __init__(self, data, **kwargs):
+    def __init__(self, data=None, **kwargs):
         super().__init__(**kwargs)
         if data:
             self.name = data.get("name", "")
