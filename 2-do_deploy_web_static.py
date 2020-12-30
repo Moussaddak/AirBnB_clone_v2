f0dc5ba4b3f1c66bcd3998fa43dad5b0b820b149
moussaddak <meddeb.moussaddak@gmail.com>
2020-12-30 00:19:46 +0000

    another hotfix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ Fabric module """
-from fabric.api import env, put, run
+from fabric.api import env, put, run, local
 from os.path import exists
 from re import search
+from datetime import datetime
+import os
 
 
 env.hosts = ['ubuntu@35.237.13.104', 'ubuntu@35.231.190.248']
@@ -27,3 +29,15 @@ def do_deploy(archive_path):
                 except:
                         return False
         return False
+
+
+def do_pack():
+    """ function do_pack """
+    time = datetime.now()
+    path = "versions/web_static_{}.tgz".format(time.strftime("%Y%m%d%H%M%S"))
+    if not os.path.exists("versions/"):
+        os.mkdir("versions")
+    result = local("tar -cvzf {} web_static".format(path), capture=True)
+    if result.succeeded:
+        return path
+    return None
