297ddee5f12fb54345482999d7be61793db4612a
moussaddak <meddeb.moussaddak@gmail.com>
2021-01-20 17:41:38 +0000

    pythoniscool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Python is fun! """
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def say_hello():
+    """ print Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def HBNB():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def Cisfun(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def Pythonisfun(text=None):
+    if text:
+        return "Python {}".format(text.replace('_', ' '))
+    return 'Python is cool'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
